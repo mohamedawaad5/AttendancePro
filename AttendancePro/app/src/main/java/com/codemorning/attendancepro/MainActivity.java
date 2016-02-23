a8731619c38c6ef4d4df686d1bbfce730386aa80
Mohamed Awad <mohamed.awaad5@gmail.com>
2016-02-23 20:35:59 +0200

    ss

---
+++ AttendancePro/app/src/main/java/com/codemorning/attendancepro/MainActivity.java
@@ -0,0 +1,134 @@
+package com.codemorning.attendancepro;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<Note> posts;
+    ArrayAdapter<Note> adapter;
+    ListView lv;
+    ProgressBar pb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser == null) {
+            loadLoginView();
+        }
+
+        pb = (ProgressBar)findViewById(R.id.progressbar);
+        posts = new ArrayList<Note>();
+        adapter = new ArrayAdapter<Note>(this, R.layout.list_item_layout, posts);
+        lv = (ListView)findViewById(R.id.list);
+
+        lv.setAdapter(adapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Note note = posts.get(position);
+                Intent intent = new Intent(MainActivity.this, EditNoteActivity.class);
+                intent.putExtra("noteId", note.getId());
+                intent.putExtra("noteTitle", note.getTitle());
+                intent.putExtra("noteContent", note.getContent());
+                startActivity(intent);
+            }
+        });
+
+        refreshPostList();
+
+    }
+
+
+    private void refreshPostList() {
+        pb.setVisibility(View.VISIBLE);
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Post");
+        query.whereEqualTo("author", ParseUser.getCurrentUser());
+
+        query.findInBackground(new FindCallback<ParseObject>() {
+
+            @Override
+            public void done(List<ParseObject> postList, ParseException e) {
+                pb.setVisibility(View.GONE);
+                if (e == null) {
+                    // If there are results, update the list of posts
+                    // and notify the adapter
+                    posts.clear();
+                    for (ParseObject post : postList) {
+                        Note note = new Note(post.getObjectId(), post.getString("title"), post.getString("content"));
+                        posts.add(note);
+                    }
+                    adapter.notifyDataSetChanged();
+                } else {
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+
+            case R.id.action_refresh: {
+                refreshPostList();
+                break;
+            }
+
+            case R.id.action_new: {
+                Intent intent = new Intent(this, EditNoteActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.action_logout: {
+                ParseUser.logOut();
+                loadLoginView();
+                break;
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    private void loadLoginView() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+}
